e2dbd362854d4ea5817b12d658f6dc80c8e81a72
torexxx <molento-studio@yandex.ru>
2021-02-18 19:38:44 +0700

    accordion fix

--- dist/script.js
+++ dist/script.js
@@ -4677,7 +4677,7 @@ window.addEventListener('DOMContentLoaded', function () {
   Object(_modules_calc__WEBPACK_IMPORTED_MODULE_6__["default"])('#size', '#material', '#options', '.promocode', '.calc-price');
   Object(_modules_filter__WEBPACK_IMPORTED_MODULE_7__["default"])();
   Object(_modules_pictureSize__WEBPACK_IMPORTED_MODULE_8__["default"])('.sizes-block');
-  Object(_modules_accordion__WEBPACK_IMPORTED_MODULE_9__["default"])('.accordion-heading', '.accordion-block');
+  Object(_modules_accordion__WEBPACK_IMPORTED_MODULE_9__["default"])('.accordion-heading');
 });
 
 /***/ }),
@@ -4698,17 +4698,25 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-var accordion = function accordion(triggersSelector, itemsSelector) {
+var accordion = function accordion(triggersSelector) {
   var btns = document.querySelectorAll(triggersSelector);
   btns.forEach(function (btn) {
-    btn.addEventListener('click', function () {
-      this.classList.toggle('active-style');
-      this.nextElementSibling.classList.toggle('active-content');
+    btn.addEventListener("click", function () {
+      if (!this.classList.contains("active-style")) {
+        btns.forEach(function (btn) {
+          btn.classList.remove("active-style");
+          btn.nextElementSibling.classList.remove("active-content");
+          btn.nextElementSibling.style.maxHeight = "0px";
+        });
+      }
 
-      if (this.classList.contains('active-style')) {
+      this.classList.toggle("active-style");
+      this.nextElementSibling.classList.toggle("active-content");
+
+      if (this.classList.contains("active-style")) {
         this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + "px";
       } else {
-        this.nextElementSibling.style.maxHeight = '0px';
+        this.nextElementSibling.style.maxHeight = "0px";
       }
     });
   }); // blocks = document.querySelectorAll(itemsSelector);
